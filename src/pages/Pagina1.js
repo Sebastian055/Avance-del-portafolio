9795a88eb0766f20ee75c414dfb72da904ebec84
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-11-15 23:38:26 -0500

    Portafolio Trabajo Sebastian Cifuentes

--- src/pages/Pagina1.js
+++ src/pages/Pagina1.js
@@ -1,27 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import "./Pagina1.css"; 
+import "./Pagina1.css";
 
 function Pagina1() {
-  const [backgroundColor, setBackgroundColor] = useState("#1f1d1d");
-
-  const handleMouseEnter = () => {
-    setBackgroundColor("#333");
-  };
-
-  const handleMouseLeave = () => {
-    setBackgroundColor("#1f1d1d");
-  };
-
   return (
-    <div
-      className="pagina1"
-      style={{ backgroundColor }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <h1 className="title">Portafolio</h1>
-      <div className="nombre">Sebastian Cifuentes</div>
+    <div className="pagina1">
+      <h1 className="title">PORTFOLIO</h1>
+      <div className="nombre">SEBASTIAN CIFUENTES</div>
       <Link to="/Pagina2" className="siguientepag">
         Siguiente página
       </Link>
