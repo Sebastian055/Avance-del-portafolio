370dacaec7aab0b03a0144e69ad45ccc86ceca2d
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-11-15 23:45:36 -0500

    Portafolio Trabajo Sebastian Cifuentes

--- src/pages/Pagina2.js
+++ src/pages/Pagina2.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Pagina2.css'; 
+import "./Pagina2.css";
 
 function Pagina2() {
   return (
     <div className="contenidoo">
-      <div className="title">Mi portafolio</div>
-      <div className="subtitulo">
-        Hola, soy Sebastian Cifuentes Moncada, bienvenido a mi portafolio.
-      </div>
+      <h1 className="title">BIOGRAFÍA</h1>
+      <div className="subtitulo">Hola,soy Sebastian Cifuentes Moncada, Bienvenido a mi portafolio</div>
       <div className="contenido">
         Hola, me presento soy Sebastian Cifuentes Moncada, un estudiante de la
         institución universitaria Pascual Bravo. En estos momentos estoy
         realizando mi propio portafolio para mi página web.
       </div>
-      <div className="navegacion">
-        <Link to="/Pagina3" className="siguientepag">
-          Siguiente página
-        </Link>
-        <Link to="/Pagina1" className="anteriorpag">
-          Página anterior
-        </Link>
-      </div>
+      <Link to="/Pagina1" className="anteriorpag">
+        Página anterior
+      </Link>
+      <Link to="/Pagina3" className="siguientepag">
+        Siguiente página
+      </Link>
     </div>
   );
 }
