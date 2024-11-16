99a3c002bdf7b0a72102aff838d11ab0a71b6640
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-11-15 22:44:17 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-// Importar las páginas
 import Pagina1 from './pages/Pagina1';
 import Pagina2 from './pages/Pagina2';
 import Pagina3 from './pages/Pagina3';
@@ -15,7 +14,6 @@ function App() {
         <Route path="/Pagina2" element={<Pagina2 />} />
         <Route path="/Pagina3" element={<Pagina3 />} />
         <Route path="/Pagina4" element={<Pagina4 />} />
-        {/* Redirigir al inicio por defecto */}
         <Route path="/" element={<Navigate to="/Pagina1" />} />
       </Routes>
     </Router>
